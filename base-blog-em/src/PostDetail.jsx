41c289b7d29ec04a04d7d5af108b46b9ccda38a3
devJaehwan <fodore12@gmail.com>
2025-03-21 16:33:08 +0900

    feat: 블로그 댓글 쿼리 추가

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,9 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import { fetchComments } from "./api";
 import "./PostDetail.css";
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["postComments", post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <div>error</div>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
