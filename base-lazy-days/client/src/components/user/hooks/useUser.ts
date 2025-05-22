5f4ca674a292f1d76ddbed6a59da6930faf54223
devJaehwan <fodore12@gmail.com>
2025-05-22 19:47:11 +0900

    feat: useUser 훅 setQueryData 및 removeQueries 적용

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,3 +1,4 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 
 import type { User } from "@shared/types";
@@ -5,7 +6,6 @@ import type { User } from "@shared/types";
 import { useLoginData } from "@/auth/AuthContext";
 import { axiosInstance, getJWTHeader } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
-import { useQuery } from "@tanstack/react-query";
 import { generateUserKey } from "@/react-query/key-factories";
 
 // query function
@@ -21,6 +21,8 @@ async function getUser(userId: number, userToken: string) {
 }
 
 export function useUser() {
+  const queryClient = useQueryClient();
+
   const { userId, userToken } = useLoginData();
 
   // TODO: call useQuery to update user data from server
@@ -33,12 +35,15 @@ export function useUser() {
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
+    queryClient.setQueryData(
+      generateUserKey(newUser.id, newUser.token),
+      newUser
+    );
   }
 
   // meant to be called from useAuth
   function clearUser() {
-    // TODO: reset user to null in query cache
+    queryClient.removeQueries({ queryKey: [queryKeys.user] });
   }
 
   return { user, updateUser, clearUser };
