26c100e02deb2734fd5e28fb6b46e6cf0e796b8d
devJaehwan <fodore12@gmail.com>
2025-05-23 16:18:32 +0900

    feat: usePatchUser hook 낙관적 업데이트 추가

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,3 +1,5 @@
+import { useToast } from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import jsonpatch from "fast-json-patch";
 
 import type { User } from "@shared/types";
@@ -5,33 +7,46 @@ import type { User } from "@shared/types";
 import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 import { useUser } from "./useUser";
 
-// for when we need a server function
-// async function patchUserOnServer(
-//   newData: User | null,
-//   originalData: User | null,
-//  ): Promise<User | null> {
-//   if (!newData || !originalData) return null;
-//   // create a patch for the difference between newData and originalData
-//   const patch = jsonpatch.compare(originalData, newData);
+import { queryKeys } from "@/react-query/constants";
 
-//   // send patched data to the server
-//   const { data } = await axiosInstance.patch(
-//     `/user/${originalData.id}`,
-//     { patch },
-//     {
-//       headers: getJWTHeader(originalData.token),
-//     },
-//   );
-//   return data.user;
-//  }
+export const MUTATION_KEY = "patch-user";
+
+// for when we need a server function
+async function patchUserOnServer(
+  newData: User | null,
+  originalData: User | null
+): Promise<User | null> {
+  if (!newData || !originalData) return null;
+  // create a patch for the difference between newData and originalData
+  const patch = jsonpatch.compare(originalData, newData);
+
+  // send patched data to the server
+  const { data } = await axiosInstance.patch(
+    `/user/${originalData.id}`,
+    { patch },
+    {
+      headers: getJWTHeader(originalData.token),
+    }
+  );
+  return data.user;
+}
 
 export function usePatchUser() {
+  const queryClient = useQueryClient();
   const { user, updateUser } = useUser();
+  const toast = useToast();
 
-  // TODO: replace with mutate function
-  const patchUser = (newData: User | null) => {
-    // nothing to see here
-  };
+  const { mutate: patchUser } = useMutation({
+    mutationKey: [MUTATION_KEY],
+    mutationFn: (newData: User) => patchUserOnServer(newData, user),
+    onSuccess: () => {
+      toast({ title: "user updated!", status: "success" });
+      // updateUser(userData);
+    },
+    onSettled: () => {
+      return queryClient.invalidateQueries({ queryKey: [queryKeys.user] });
+    },
+  });
 
   return patchUser;
 }
